f67e0b4ba66c91c1e399c07a57aed242b8dd23d7
Peter Ahn <peterahn8@gmail.com>
2025-12-11 21:31:59 -0500

    add scaffolding for `Thread` implementation

---
+++ Thread.cpp
@@ -0,0 +1,21 @@
+//
+// Created by Peter Ahn on 12/11/25.
+//
+
+#include "Thread.h"
+
+void Thread::start() {
+
+}
+
+void Thread::stop() {
+
+}
+
+void Thread::done() {
+
+}
+
+int Thread::id() {
+
+}
