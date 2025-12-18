b2bcb2ae451f69801f06aecad5dd7673f67f712c
Peter Ahn <peterahn8@gmail.com>
2025-12-17 23:31:27 -0500

    use pthread mutex

--- CountingThread.cpp
+++ CountingThread.cpp
@@ -6,6 +6,10 @@ CountingThread::CountingThread(GlobalState& globalState, int index)
 }
 
 void CountingThread::doWork() {
+    pthread_mutex_lock(&_globalState.mutex); // lock global state to prevent other threads from reading/writing it
+
     std::cout << "Hello from CountingThread " << _index << 
     " (pthread id: " << id() << ")" << std::endl;
+
+    pthread_mutex_unlock(&_globalState.mutex); // unlock global state
 }
--- main.cpp
+++ main.cpp
@@ -2,15 +2,16 @@
 #include "GlobalState.h"
 #include "CountingThread.h"
 
-int main()
-{
+int main() {
     GlobalState globalState;
 
-    // std::cout << "Hello, World!" << std::endl;
+    pthread_mutex_init(&globalState.mutex, nullptr); // initialize mutex
 
     CountingThread thread0(globalState, 0);
     thread0.start();
     thread0.join();
+
+    pthread_mutex_destroy(&globalState.mutex); // destroy mutex.. Not necessary with this short lifecycle, but docs recommended it
 
     return 0;
 }
