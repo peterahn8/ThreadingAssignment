a3cc1b1d1f54dd6af0f6a0187df07cefefffd942
Peter Ahn <peterahn8@gmail.com>
2025-12-18 23:49:06 -0500

    use mersenne twister for rng

--- CountingThread.cpp
+++ CountingThread.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <random>
+#include <thread>
 #include "CountingThread.h"
 
 CountingThread::CountingThread(GlobalState& globalState, int index)
@@ -6,23 +8,28 @@ CountingThread::CountingThread(GlobalState& globalState, int index)
 }
 
 void CountingThread::doWork() {
+    // https://en.cppreference.com/w/cpp/numeric/random/mersenne_twister_engine.html
+    std::mt19937 rng(std::random_device{}() + _index);
+    std::uniform_int_distribution<int> dist(1, 10);
 
     while (!stopRequested) {
-        int step = 1;
+        int increment = dist(rng);
 
         pthread_mutex_lock(&_globalState.mutex);
 
         int remaining = 100 - _globalState.total;
-        if (remaining == 0) {
+        if (remaining <= 0) {
             pthread_mutex_unlock(&_globalState.mutex);
             stop();
             break;
         }
 
-        _count += step;
-        _globalState.total += step;
+        _count += increment;
+        _globalState.total += increment;
 
         pthread_mutex_unlock(&_globalState.mutex);
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(1)); // the work is so fast/cheap that if I don't manually sleep the thread, then the same thread hogs the cpu
     }
 }
 
